8c15085b91281667bca6ab02935c4aed2aeae5eb
Benny Degezelle <hi@monkeypatch.be>
2011-07-29 14:16:24 +0200

    make it possible to specify a root for related pages options

--- app/helpers/relations_helper.rb
+++ app/helpers/relations_helper.rb
@@ -2,10 +2,15 @@ module RelationsHelper
   def relation_options(selected = nil)
     options = []
     options << [t('support.select.prompt'), nil]
-    Page.all.each do |p| 
-      options << [p.title, p.id]
+    if @page && @page.field('relations_target_parent_id')
+      Page.find(@page.field('relations_target_parent_id').content).children.each do |p| 
+        options << [p.title, p.id]
+      end
+    else
+      Page.all.each do |p| 
+        options << [p.title, p.id]
+      end
     end
-    
     options_for_select(options, selected)
   end
 end
