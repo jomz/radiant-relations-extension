8dc93637000dc9bf3bfe3e95c7b40648724098e2
Benny Degezelle <hi@monkeypatch.be>
2012-09-25 21:25:35 +0200

    Make r:related_pages only expand if there are any, add r:related_pages:first/last

--- lib/relations_extension/relations_tags.rb
+++ lib/relations_extension/relations_tags.rb
@@ -1,18 +1,28 @@
 module RelationsExtension::RelationsTags
   include Radiant::Taggable
 
-  desc %{
-    
-  }
   tag 'related_pages' do |tag|
-    tag.expand
-  end
-  tag 'related_pages:each' do |tag|
     related_pages = tag.locals.page.related_pages
-    
+    tag.expand unless related_pages.empty?
+  end
+  desc %{
+    Iterates over all related pages. Page relations should be declared via a Relations page part.
+  }
+  tag 'related_pages:each' do |tag|
     related_pages.inject('') do |result, related_page|
       tag.locals.page = related_page
       result + tag.expand
     end
+  end
+  
+  desc %{ }
+  tag 'related_pages:first' do |tag|
+    tag.locals.page = related_pages.first
+    tag.expand
+  end
+  desc %{ }
+  tag 'related_pages:last' do |tag|
+    tag.locals.page = related_pages.last
+    tag.expand
   end
 end
