2cb9a9c785a17c7cc3c2558c9e652a86d6208869
Benny Degezelle <hi@monkeypatch.be>
2012-09-25 20:20:55 +0200

    Update tree builder to be more user-friendly and configurable

--- app/helpers/relations_helper.rb
+++ app/helpers/relations_helper.rb
@@ -3,14 +3,24 @@ module RelationsHelper
     options = []
     options << [t('support.select.prompt'), nil]
     if @page && @page.field('relations_target_parent_id')
-      Page.find(@page.field('relations_target_parent_id').content).children.each do |p| 
-        options << [p.title, p.id]
-      end
+      options.concat build_tree(Page.find(@page.field('relations_target_parent_id').content), [])
     else
-      Page.all.each do |p| 
-        options << [p.title, p.id]
-      end
+      options.concat build_tree(Page.root, [])
     end
     options_for_select(options, selected)
+  end
+  
+  def build_tree(page, list, level = 0)
+    label = "#{'-'*level} #{page.title}"
+    id = page.id
+    list << [label, id]
+
+    return list if page.fields.select{|f| f.name == "exclude_children_from_relations_target"}.any?
+    return list if Radiant::Config["relations.exclude_archive_children"] && page.class_name =~ /ArchivePage/
+
+    page.children.each do |p|
+      build_tree p, list, level + 1
+    end
+    list
   end
 end
