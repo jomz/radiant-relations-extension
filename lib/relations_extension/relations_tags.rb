c8374addd5171f82bdc21bc58b581aae08a01b5e
Benny Degezelle <hi@monkeypatch.be>
2012-09-27 17:22:54 +0200

    Update radius tags, add from_ancestor param to r:related_pages

--- lib/relations_extension/relations_tags.rb
+++ lib/relations_extension/relations_tags.rb
@@ -2,14 +2,21 @@ module RelationsExtension::RelationsTags
   include Radiant::Taggable
 
   tag 'related_pages' do |tag|
-    related_pages = tag.locals.page.related_pages
-    tag.expand unless related_pages.empty?
+    tag.locals.related_pages = tag.locals.page.related_pages
+    if tag.attr['from_ancestor']
+      ancestor_path = Page.find(tag.attr['from_ancestor']).path
+      tag.locals.related_pages.delete_if{|p| p.path =~ /^#{ancestor_path}/ }
+    end
+    tag.expand unless tag.locals.related_pages.empty?
   end
   desc %{
     Iterates over all related pages. Page relations should be declared via a Relations page part.
+    You can limit the results by use of a @from_ancestor@ attribute. 
+    For example: &lt;r:related_pages from_ancestor="8"&gt;&lt;r:each:link/&gt;&lt;/r:related_pages&gt; where 8 is a page id
+    The from_ancestor attribute accepts only one page id.
   }
   tag 'related_pages:each' do |tag|
-    related_pages.inject('') do |result, related_page|
+    tag.locals.related_pages.inject('') do |result, related_page|
       tag.locals.page = related_page
       result + tag.expand
     end
@@ -17,12 +24,12 @@ module RelationsExtension::RelationsTags
   
   desc %{ }
   tag 'related_pages:first' do |tag|
-    tag.locals.page = related_pages.first
+    tag.locals.page = tag.locals.related_pages.first
     tag.expand
   end
   desc %{ }
   tag 'related_pages:last' do |tag|
-    tag.locals.page = related_pages.last
+    tag.locals.page = tag.locals.related_pages.last
     tag.expand
   end
 end
