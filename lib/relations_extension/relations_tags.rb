b581761b50f51ecf42d0b80a87367f9ac4bdc2b4
jomz <hi@monkeypatch.be>
2019-06-06 14:36:54 +0200

    Add r:if/unless_related_pages tags

--- lib/relations_extension/relations_tags.rb
+++ lib/relations_extension/relations_tags.rb
@@ -11,7 +11,7 @@ module RelationsExtension::RelationsTags
   end
   desc %{
     Iterates over all related pages. Page relations should be declared via a Relations page part.
-    You can limit the results by use of a @from_ancestor@ attribute. 
+    You can limit the results by use of a @from_ancestor@ attribute.
     For example: &lt;r:related_pages from_ancestor="8"&gt;&lt;r:each:link/&gt;&lt;/r:related_pages&gt; where 8 is a page id
     The from_ancestor attribute accepts only one page id.
   }
@@ -21,12 +21,23 @@ module RelationsExtension::RelationsTags
       result + tag.expand
     end
   end
-  
+
+  desc %{ Expands only if there are related pages. }
+  tag 'if_related_pages' do |tag|
+    tag.expand if tag.locals.page.related_pages.any?
+  end
+
+  desc %{ Expands only if there are no related pages. }
+  tag 'unless_related_pages' do |tag|
+    tag.expand unless tag.locals.page.related_pages.any?
+  end
+
   desc %{ Sets the scope to the first related page. }
   tag 'related_pages:first' do |tag|
     tag.locals.page = tag.locals.related_pages.first
     tag.expand
   end
+
   desc %{ Sets the scope to the last related page. }
   tag 'related_pages:last' do |tag|
     tag.locals.page = tag.locals.related_pages.last
